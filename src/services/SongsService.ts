d6f93348bfce87faa08901d4ebfe1d4a0d9159cc
michaelpaulquimson <michaelpaulquimson@gmail.com>
2021-04-05 21:00:13 +0800

    Update SongsService.ts

--- src/services/SongsService.ts
+++ src/services/SongsService.ts
@@ -1,11 +1,39 @@
 import { Request, Response, NextFunction } from 'express';
 const Song = require('../models/songs');
 
+interface QueryParamsType  {
+	name?: string | { $regex: RegExp },
+	artist?: string | { $regex: RegExp },
+	genre?: string | { $regex: RegExp }
+}
+
 const SongsService = {
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 	getAllSongs: async (req: Request, res: Response, next: NextFunction) => {
 		try {
 			const songList = await Song.find();
+
+			if (!songList) return res.status(400).json({ status: 400, message: 'Something went wrong getting the songs' });
+			if (songList.length === 0) return res.status(200).json({ status: 200, message: 'No songs found' });
+
+			res.status(200).json({ status: 200, songs: songList });
+		} catch (err) {
+			next(err);
+		}
+	},
+	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+	filterSongsByQueryParams: async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { name, artist, genre } = req.query;
+			const queryParams: QueryParamsType = {};
+
+			if (!name && !artist && !genre) return res.status(400).json({ status: 400, message: 'Missing search criteria filter information' });
+
+			if (name) queryParams.name = { $regex : new RegExp(name.toString(), 'i') };
+			if (artist) queryParams.artist = { $regex : new RegExp(artist.toString(), 'i') };
+			if (genre) queryParams.genre = { $regex : new RegExp(genre.toString(), 'i') };
+
+			const songList = await Song.find(queryParams).limit(5);
 
 			if (!songList) return res.status(400).json({ status: 400, message: 'Something went wrong getting the songs' });
 			if (songList.length === 0) return res.status(200).json({ status: 200, message: 'No songs found' });
