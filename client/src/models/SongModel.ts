a5619dbec84b4b37094b33bef455dc11d519ae52
michaelpaulquimson <michaelpaulquimson@gmail.com>
2021-04-07 01:13:56 +0800

    Create SongModel.ts

---
+++ client/src/models/SongModel.ts
@@ -0,0 +1,22 @@
+export interface SongProps {
+	_id: string,
+	name: string,
+	artist: string,
+	genre: string
+}
+
+class Song {
+	_id: string;
+	name: string;
+	artist: string;
+	genre: string;
+
+	constructor({ _id, name, artist, genre }: SongProps) {
+		this._id = _id;
+		this.name = name;
+		this.artist = artist;
+		this.genre = genre;
+	}
+}
+
+export default Song;
