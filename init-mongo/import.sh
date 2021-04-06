809c15e53f5b8a87899253756a9c3d43f914d098
michaelpaulquimson <michaelpaulquimson@gmail.com>
2021-04-07 01:29:06 +0800

    Add initial data in mongoDB

---
+++ init-mongo/import.sh
@@ -0,0 +1,1 @@
+mongoimport --db songs --collection songs --type json --file ./docker-entrypoint-initdb.d/data.json --jsonArray
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoUri: process.env.MONGO_URI || 'mongodb://admin:pass@mongo:27017/musics',
+	mongoUri: process.env.MONGO_URI || 'mongodb://admin:pass@mongo:27017/songs',
 	PORT: process.env.PORT || 3000
 };
