1576ae8dcf3d25318797dbafa99fd321d373da8e
michaelpaulquimson <michaelpaulquimson@gmail.com>
2021-04-07 18:04:34 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import SongContainer from './containers/SongContainer';
 import SpotifyContainer from './containers/SpotifyContainer';
+import { Container } from './components/layouts/Styles';
 
 const App: React.FC = () => {
 	return(
-		<>
+		<Container>
 			<SpotifyContainer />
 			<SongContainer />
-		</>
+		</Container>
 	);
 };
 
