23da08e102d731e7bb0642d484f825dd265a746e
michaelpaulquimson <michaelpaulquimson@gmail.com>
2021-04-07 17:14:02 +0800

    Create LayoutStyles.ts

---
+++ client/src/components/layouts/LayoutStyles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+	margin: 8px;
+`;
+
+export const Header = styled.h1`
+	font-weight: bold;
+	font-size: 30px;
+	margin: 8px;
+`;
