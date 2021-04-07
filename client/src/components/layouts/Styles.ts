1482e0df1fa6579ee5c39f02629f89a693167c1e
michaelpaulquimson <michaelpaulquimson@gmail.com>
2021-04-07 18:04:42 +0800

    Update Styles.ts

--- client/src/components/layouts/Styles.ts
+++ client/src/components/layouts/Styles.ts
@@ -11,4 +11,14 @@ export const Header = styled.h1`
 	font-weight: bold;
 	font-size: 30px;
 	margin: 8px;
+	text-align: center;
+`;
+
+export const Button = styled.button`
+	width: 200px;
+	height: 50px;
+	border-radius: 6px;
+	cursor: pointer;
+	background-color: #4CAF50;
+	color: white;
 `;
