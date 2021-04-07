199082087cc9abe943b060a7f74997905f232ae7
michaelpaulquimson <michaelpaulquimson@gmail.com>
2021-04-07 13:18:50 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import SongContainer from './containers/SongContainer';
+import SpotifyContainer from './containers/SpotifyContainer';
 
 const App: React.FC = () => {
 	return(
 		<>
+			<SpotifyContainer />
 			<SongContainer />
 		</>
 	);
